3d3f1f1ac1056db8c20b838b97f2decb48ae9400
hosuak <138014804+hosuak@users.noreply.github.com>
2023-06-28 21:31:45 +0900

    HelloJava

---
+++ HelloJava.java
@@ -0,0 +1,33 @@
+package folder;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+
+		// 주석
+		// 주석은 프로그램 실행에 영향을 주지 않는다.
+		// 주석은 소스내용 설명을 위한 코드이다. (즉 개발자를 위한 코드)
+		// 한줄주석
+		/* 범위주석 */
+		
+		System.out.println("Hello world ~!!!");
+		// Hello World ~!!! 를 출력해주는 코드
+
+		System.out.println("/*주석이 될 수 없습니다.*/");
+
+		// 이클립스에서 자주 사용하는 단축키
+		// tab : 들여쓰기
+		// shift + tap :내어쓰기
+		// ctrl + shift + c : 라인주석 단축키
+		// ctrl + (+, -) : 폰트 크기조절 단축키
+		// ctrl + space : 자동완성 단축키
+		// 예) sysout 후 ctrl + space >>> System.out.printLn();
+		// ctrl + shift + f : 자동정렬 단축키
+		// ctrl + s : 자동저장
+		// crtl + f11 : 실행 단축키
+		
+		System.out.println();
+
+	}
+
+}
